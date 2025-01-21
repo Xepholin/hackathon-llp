f7799179ac028e53fccd517d121abd8b35012bd5
Martin DIAMANT <martin.diamant@ens.uvsq.fr>
2025-01-21 22:01:38 +0100

    Added some files for execution

---
+++ serial/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=test_optim       # Nom de la tâche
+#SBATCH --nodes=1                # Nombre de noeuds
+#SBATCH --ntasks=1               # Nombre de tâches
+# #SBATCH --cpus-per-task=4        # Nombre de processeurs par tâche
+#SBATCH --mem=1G                 # Mémoire par tâche
+#SBATCH --time=00:10:00          # Temps limite (HH:MM:SS)
+#SBATCH --partition=c8g # Partition spécifique
+
+# Chargez les modules nécessaires (facultatif, selon votre configuration)
+# module load module1
+# module load module2
+
+# Commandes a exécuter
+# Les lignes s'exécutent une à une
+
+OMP_PLACES=cores OMP_PROC_BIND=close ./maqao OV -R1 --executable="tested_program.exe" --run-command="<executable> 1000000 1000"
