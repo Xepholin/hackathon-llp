9e4f1abce64a86a2ff02dd7364fd082f4e6bcbaa
Martin DIAMANT <martin.diamant@ens.uvsq.fr>
2025-01-23 11:42:22 +0100

    Updated scripts

--- serial/start.sh
+++ serial/start.sh
@@ -3,15 +3,16 @@
 #SBATCH --nodes=1                # Nombre de noeuds
 #SBATCH --ntasks=1               # Nombre de tâches
 # #SBATCH --cpus-per-task=4        # Nombre de processeurs par tâche
-#SBATCH --mem=1G                 # Mémoire par tâche
+#SBATCH --mem=10G                 # Mémoire par tâche
 #SBATCH --time=00:10:00          # Temps limite (HH:MM:SS)
 #SBATCH --partition=c8g # Partition spécifique
 
 # Chargez les modules nécessaires (facultatif, selon votre configuration)
 # module load module1
 # module load module2
+module load openmpi5
 
-# Commandes a exécuter
-# Les lignes s'exécutent une à une
+# Commandes a exécuter sur chaque n�[m~Sud
+# (remplacez ces lignes par les commandes réelles de votre script)
 
-OMP_PLACES=cores OMP_PROC_BIND=close OMP_NUM_THREADS=96 ./maqao OV -R1 --executable="tested_program.exe" --run-command="<executable> 1000000 1000"
+OMP_PLACES=cores OMP_PROC_BIND=close OMP_NUM_THREADS=96 OMP_SCHEDULE=static,10000 ./maqao OV -R1 --executable="tested_program.exe" --run-command="<executable> 1000000 1000"
