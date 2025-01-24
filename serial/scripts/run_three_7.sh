b04baa440286cdafd0b5cbc65e4310de136a57e9
Martin DIAMANT <martin.diamant@ens.uvsq.fr>
2025-01-24 01:45:49 +0100

    Fixed typo on scripts
    They had the number of cores for Graviton4 and not Graviton3

--- serial/scripts/run_three_7.sh
+++ serial/scripts/run_three_7.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=prog_c7g       # Nom de la tâche
 #SBATCH --nodes=1                # Nombre de noeuds
 #SBATCH --ntasks=1               # Nombre de tâches
-#SBATCH --cpus-per-task=96        # Nombre de processeurs par tâche
+#SBATCH --cpus-per-task=64        # Nombre de processeurs par tâche
 #SBATCH --threads-per-core=1
 #SBATCH --mem=90G                 # Mémoire par tâche
 #SBATCH --time=24:00:00          # Temps limite (HH:MM:SS)
@@ -16,5 +16,5 @@ module load openmpi5
 # Commandes a exécuter sur chaque n�[m~Sud
 # (remplacez ces lignes par les commandes réelles de votre script)
 
-export SRUN_CPUS_PER_TASK=96
-OMP_NUM_THREADS=96 OMP_PLACES=cores OMP_PROC_BIND=close ./c7g.exe 10000000 1000000
+export SRUN_CPUS_PER_TASK=64
+OMP_NUM_THREADS=64 OMP_PLACES=cores OMP_PROC_BIND=close ./c7g.exe 10000000 1000000
